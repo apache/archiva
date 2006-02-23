28d269cb00132d8765129ff6be70cd7c7fd45412
Edwin L. Punzalan <epunzalan@apache.org>
2006-02-23 03:27:56 +0000

    replaced usage of FileUtils.contentEquals() to private method compareFiles()
    git-svn-id: https://svn.apache.org/repos/asf/maven/repository-manager/trunk@380034 13f79535-47bb-0310-9956-ffa450edef68

--- maven-repository-converter/src/test/java/org/apache/maven/repository/converter/RepositoryConverterTest.java
+++ maven-repository-converter/src/test/java/org/apache/maven/repository/converter/RepositoryConverterTest.java
@@ -208,13 +208,13 @@ public class RepositoryConverterTest
         Artifact pomArtifact = createArtifact( "relocated-test", "relocated-v3artifact", "1.0.0", "1.0.0", "pom" );
         File pomFile = getTestFile( "src/test/expected-files/" + targetRepository.pathOf( pomArtifact ) );
         File testFile = getTestFile( "target/test-target-repository/" + targetRepository.pathOf( pomArtifact ) );
-        assertTrue( "Check if expected relocated pom matches", FileUtils.contentEquals( pomFile, testFile ) );
+        compareFiles( pomFile, testFile );
 
         Artifact orig = createArtifact( "test", "relocated-v3artifact", "1.0.0", "1.0.0", "pom" );
         artifactFile = new File( targetRepository.getBasedir(), targetRepository.pathOf( orig ) );
         assertTrue( "Check if relocation artifact pom is created", artifactFile.exists() );
         testFile = getTestFile( "src/test/expected-files/" + targetRepository.pathOf( orig ) );
-        assertTrue( "Check if expected artifact matches", FileUtils.contentEquals( artifactFile, testFile ) );
+        compareFiles( artifactFile, testFile );
     }
 
     public void testV3PomWarningsOnConvert()
