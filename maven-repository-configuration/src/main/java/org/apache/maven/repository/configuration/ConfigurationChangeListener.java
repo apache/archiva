e764c61109b68e47f3d10e1ff6270446a4d24f19
Brett Porter <brett@apache.org>
2006-07-10 04:39:44 +0000

    [MRM-46] set application up so that configuration can be loaded from a webapp context param (using expressions)
    git-svn-id: https://svn.apache.org/repos/asf/maven/repository-manager/trunk@420421 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-repository-configuration/src/main/java/org/apache/maven/repository/configuration/ConfigurationChangeListener.java
@@ -0,0 +1,33 @@
+package org.apache.maven.repository.configuration;
+
+/*
+ * Copyright 2005-2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Component capable of noticing configuration changes and adjusting accordingly.
+ * This is not a Plexus role.
+ *
+ * @author <a href="mailto:brett@apache.org">Brett Porter</a>
+ */
+public interface ConfigurationChangeListener
+{
+    /**
+     * Notify the object that there has been a configuration change.
+     *
+     * @param configuration the new configuration
+     */
+    void notifyOfConfigurationChange( Configuration configuration );
+}
---
+++ maven-repository-configuration/src/main/java/org/apache/maven/repository/configuration/DefaultConfigurationStore.java
@@ -0,0 +1,119 @@
+package org.apache.maven.repository.configuration;
+
+/*
+ * Copyright 2005-2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.maven.repository.configuration.io.xpp3.ConfigurationXpp3Reader;
+import org.codehaus.plexus.logging.AbstractLogEnabled;
+import org.codehaus.plexus.util.IOUtil;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Load and store the configuration. No synchronization is used, but it is unnecessary as the old configuration object
+ * can continue to be used.
+ *
+ * @author <a href="mailto:brett@apache.org">Brett Porter</a>
+ * @todo would be great for plexus to do this for us - so the configuration would be a component itself rather than this store
+ * @todo would be good to monitor the store file for changes
+ * @todo support other implementations than XML file
+ * @plexus.component role="org.apache.maven.repository.configuration.ConfigurationStore" role-hint="default"
+ */
+public class DefaultConfigurationStore
+    extends AbstractLogEnabled
+    implements ConfigurationStore
+{
+    /**
+     * @plexus.configuration default-value="${configuration.store.file}"
+     */
+    private File file;
+
+    /**
+     * The cached configuration.
+     */
+    private Configuration configuration;
+
+    /**
+     * List of listeners to configuration changes.
+     */
+    private List/*<ConfigurationChangeListener>*/ listeners = new LinkedList();
+
+    public Configuration getConfigurationFromStore()
+        throws ConfigurationStoreException
+    {
+        if ( configuration == null )
+        {
+            ConfigurationXpp3Reader reader = new ConfigurationXpp3Reader();
+
+            if ( file == null )
+            {
+                file = new File( System.getProperty( "user.home" ), "/.m2/repository-manager.xml" );
+            }
+
+            FileReader fileReader;
+            try
+            {
+                fileReader = new FileReader( file );
+            }
+            catch ( FileNotFoundException e )
+            {
+                getLogger().warn( "Configuration file: " + file + " not found. Using defaults." );
+                configuration = new Configuration();
+                return configuration;
+            }
+
+            try
+            {
+                configuration = reader.read( fileReader );
+            }
+            catch ( IOException e )
+            {
+                throw new ConfigurationStoreException( e.getMessage(), e );
+            }
+            catch ( XmlPullParserException e )
+            {
+                throw new ConfigurationStoreException( e.getMessage(), e );
+            }
+            finally
+            {
+                IOUtil.close( fileReader );
+            }
+        }
+        return configuration;
+    }
+
+    public void storeConfiguration( Configuration configuration )
+        throws ConfigurationStoreException
+    {
+        // TODO: finish!
+
+        for ( Iterator i = listeners.iterator(); i.hasNext(); )
+        {
+            ConfigurationChangeListener listener = (ConfigurationChangeListener) i.next();
+
+            listener.notifyOfConfigurationChange( configuration );
+        }
+
+        throw new UnsupportedOperationException( "Not yet implemented: storeConfiguration" );
+    }
+}
