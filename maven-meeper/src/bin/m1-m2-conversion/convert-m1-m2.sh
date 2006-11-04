8b9dfddeaf8de7509906d74618bf92b86c83193e
Jason van Zyl <jvanzyl@apache.org>
2006-11-04 10:18:07 +0000

    o create properties files for repository conversion using archiva
    git-svn-id: https://svn.apache.org/repos/asf/maven/archiva/trunk@471158 13f79535-47bb-0310-9956-ffa450edef68

--- maven-meeper/src/bin/m1-m2-conversion/convert-m1-m2.sh
+++ maven-meeper/src/bin/m1-m2-conversion/convert-m1-m2.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
-java -jar archiva-cli-1.0-SNAPSHOT-cli.jar -c \
- -o /home/maven/repository-staging/to-ibiblio/maven \
- -n /home/maven/repository-staging/to-ibiblio/maven2-repoclean \
- -b **/*.pom,**/activation/**,**/javamail/**,**/jaxm/**,**/jaxp/**,**/jaxrpc/**,**/jca/**,**/jce/**,**/jdbc/**,**/jdo/**,**/jms/**,**/jndi/**,**/saaj/**
+java -jar archiva-cli-1.0-SNAPSHOT-cli.jar -c synchronize.properties
+java -jar archiva-cli-1.0-SNAPSHOT-cli.jar -c synchronize-java.net.properties
+
+dir=/home/maven/repository-staging/to-ibiblio
+src=$dir/maven2-repoclean
+dst=$dir/maven2
+
+rsync --ignore-existing -rvpl $src/ $dst/ > $log
+
+for f in `cat $log | grep maven-metadata.xml$` ; do
+  md5sum $dst/$f > $dst/$f.md5;
+  sha1sum $dst/$f > $dst/$f.sha1;
+  md5sum $src/$f > $src/$f.md5;
+  sha1sum $src/$f > $src/$f.sha1;
+done
