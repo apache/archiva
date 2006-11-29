4f9e9df329d394cd404f90e55c46ceb54f894650
Jason van Zyl <jvanzyl@apache.org>
2006-11-29 18:47:24 +0000

    git-svn-id: https://svn.apache.org/repos/asf/maven/archiva/trunk@480678 13f79535-47bb-0310-9956-ffa450edef68

--- maven-meeper/src/bin/synchronize-central-to-ibiblio.sh
+++ maven-meeper/src/bin/synchronize-central-to-ibiblio.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-syncProperties=$1                                                                                                                          
-                                                                                                                                           
-. $syncProperties      
+[ "$1" = "" ] && echo && echo "You must pass in the synchronize.properties file!" && echo && exit
+
+. $1
 
 rsync -e ssh --delete --max-delete=10 -v -riplt $MAVEN2_REPO/ $IBIBLIO_SYNC_HOST:$M2_IBIBLIO_SYNC_DIR
 
