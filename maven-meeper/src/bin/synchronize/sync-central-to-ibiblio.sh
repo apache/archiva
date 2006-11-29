bef86917b5492629eb762ae11cc9a19d9abee8d1
Jason van Zyl <jvanzyl@apache.org>
2006-11-29 17:32:53 +0000

    o using all sync properties
    git-svn-id: https://svn.apache.org/repos/asf/maven/archiva/trunk@480648 13f79535-47bb-0310-9956-ffa450edef68

--- maven-meeper/src/bin/synchronize/sync-central-to-ibiblio.sh
+++ maven-meeper/src/bin/synchronize/sync-central-to-ibiblio.sh
@@ -6,10 +6,7 @@ syncProperties=$1
 
 dest=/home/maven/repository-staging/to-ibiblio
 
-rsync -e ssh --delete --max-delete=10 -v -riplt $dest/maven2/ $IBIBLIO_SYNC_HOST:$M2_IBIBLIO_SYNC_DIR
-
-date > $dest/maven2/last-sync.txt
-chmod a+r $dest/maven2/last-sync.txt
+rsync -e ssh --delete --max-delete=10 -v -riplt $MAVEN2_REPO/ $IBIBLIO_SYNC_HOST:$M2_IBIBLIO_SYNC_DIR
 
 # M1 Sync only for plugins ...
-rsync -e ssh --delete --max-delete=10 -v -riplt $dest/maven/ $IBIBLIO_SYNC_HOST:$M1_IBIBLIO_SYNC_DIR
+rsync -e ssh --delete --max-delete=10 -v -riplt $MAVEN1_REPO/ $IBIBLIO_SYNC_HOST:$M1_IBIBLIO_SYNC_DIR
