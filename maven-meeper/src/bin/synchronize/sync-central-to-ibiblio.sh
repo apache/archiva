0deacc911002bef9a96adf99942ce94568af28a1
Jason van Zyl <jvanzyl@apache.org>
2006-11-29 17:30:03 +0000

    o adding separate properites for m1/m2 sync dir at ibiblio
    git-svn-id: https://svn.apache.org/repos/asf/maven/archiva/trunk@480642 13f79535-47bb-0310-9956-ffa450edef68

--- maven-meeper/src/bin/synchronize/sync-central-to-ibiblio.sh
+++ maven-meeper/src/bin/synchronize/sync-central-to-ibiblio.sh
@@ -6,10 +6,10 @@ syncProperties=$1
 
 dest=/home/maven/repository-staging/to-ibiblio
 
-rsync -e ssh --delete --max-delete=10 -v -riplt $dest/maven2/ login.ibiblio.org:/public/html/maven2
+rsync -e ssh --delete --max-delete=10 -v -riplt $dest/maven2/ $IBIBLIO_SYNC_HOST:$M2_IBIBLIO_SYNC_DIR
 
 date > $dest/maven2/last-sync.txt
 chmod a+r $dest/maven2/last-sync.txt
 
 # M1 Sync only for plugins ...
-rsync -e ssh --delete --max-delete=10 -v -riplt $dest/maven/ login.ibiblio.org:/public/html/maven
+rsync -e ssh --delete --max-delete=10 -v -riplt $dest/maven/ $IBIBLIO_SYNC_HOST:$M1_IBIBLIO_SYNC_DIR
