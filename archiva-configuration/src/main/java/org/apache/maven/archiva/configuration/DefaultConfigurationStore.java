dc1905e16a45069d844e99a88aa65b065adcc59e
Brett Porter <brett@apache.org>
2007-02-07 22:56:33 +0000

    move sanitize to configuration class
    git-svn-id: https://svn.apache.org/repos/asf/maven/archiva/trunk@504728 13f79535-47bb-0310-9956-ffa450edef68

--- archiva-configuration/src/main/java/org/apache/maven/archiva/configuration/DefaultConfigurationStore.java
+++ archiva-configuration/src/main/java/org/apache/maven/archiva/configuration/DefaultConfigurationStore.java
@@ -20,7 +20,6 @@ package org.apache.maven.archiva.configuration;
  */
 
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang.StringUtils;
 import org.apache.maven.archiva.configuration.io.xpp3.ConfigurationXpp3Reader;
 import org.apache.maven.archiva.configuration.io.xpp3.ConfigurationXpp3Writer;
 import org.codehaus.plexus.logging.AbstractLogEnabled;
@@ -103,7 +102,7 @@ public class DefaultConfigurationStore
             try
             {
                 configuration = reader.read( fileReader, false );
-                sanitizeConfiguration( configuration );
+                configuration.sanitize();
             }
             catch ( IOException e )
             {
@@ -119,27 +118,6 @@ public class DefaultConfigurationStore
             }
         }
         return configuration;
-    }
-
-    /**
-     * Perform any Upgrades and Adjustments needed to bring configuration up to the
-     * current configuration format.
-     *
-     * @param config the configuration to upgrade and adjust.
-     */
-    private void sanitizeConfiguration( Configuration config )
-    {
-        Iterator it = config.getRepositories().iterator();
-        while ( it.hasNext() )
-        {
-            RepositoryConfiguration repo = (RepositoryConfiguration) it.next();
-
-            // Ensure that the repo.urlName is set.
-            if ( StringUtils.isEmpty( repo.getUrlName() ) )
-            {
-                repo.setUrlName( repo.getId() );
-            }
-        }
     }
 
     public void storeConfiguration( Configuration configuration )
