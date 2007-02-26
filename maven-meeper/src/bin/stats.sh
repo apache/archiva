3c6c1e511d3eea8b599a75e3b54f9cf9bc31e011
Carlos Sanchez Gonzalez <carlos@apache.org>
2007-02-26 22:33:15 +0000

    Add quotes
    git-svn-id: https://svn.apache.org/repos/asf/maven/archiva/trunk@512044 13f79535-47bb-0310-9956-ffa450edef68

--- maven-meeper/src/bin/stats.sh
+++ maven-meeper/src/bin/stats.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-JARS=`find $M2_REPO -iname *.jar | wc -l`
-POMS=`find $M2_REPO -iname *.pom | wc -l`
-BINJARS=`find $M2_REPO -iname *.jar ! -iname *-sources.jar ! -iname *-javadoc.jar | wc -l`
-SOURCES=`find $M2_REPO -iname *-sources.jar | wc -l`
-JAVADOCS=`find $M2_REPO -iname *-javadoc.jar | wc -l`
+JARS=`find $M2_REPO -iname "*.jar" | wc -l`
+POMS=`find $M2_REPO -iname "*.pom" | wc -l`
+BINJARS=`find $M2_REPO -iname "*.jar" ! -iname "*-sources.jar" ! -iname "*-javadoc.jar" | wc -l`
+SOURCES=`find $M2_REPO -iname "*-sources.jar" | wc -l`
+JAVADOCS=`find $M2_REPO -iname "*-javadoc.jar" | wc -l`
 
 echo total jars:  $JARS
 echo poms:        $POMS
