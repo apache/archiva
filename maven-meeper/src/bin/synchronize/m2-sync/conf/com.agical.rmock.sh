0fd2463c8b184710d997ba5f7e4651a4cb21438b
Carlos Sanchez Gonzalez <carlos@apache.org>
2007-05-15 18:34:07 +0000

    [MAVENUPLOAD-1454] missing last slash
    git-svn-id: https://svn.apache.org/repos/asf/maven/archiva/trunk@538275 13f79535-47bb-0310-9956-ffa450edef68

--- maven-meeper/src/bin/synchronize/m2-sync/conf/com.agical.rmock.sh
+++ maven-meeper/src/bin/synchronize/m2-sync/conf/com.agical.rmock.sh
@@ -4,4 +4,4 @@ CONTACTS="Daniel brolund <daniel.brolund@agical.com>"
 MODE=rsync_ssh
 
 FROM=mavensync@shell.sourceforge.net:/home/groups/r/rm/rmock/htdocs/repository
-GROUP_DIR=com/agical/rmock
+GROUP_DIR=com/agical/rmock/
