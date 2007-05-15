6dd144c706106bff330e291d4e6b944ddd8194d1
Carlos Sanchez Gonzalez <carlos@apache.org>
2007-05-15 19:50:50 +0000

    [MAVENUPLOAD-1526] missing last slash
    git-svn-id: https://svn.apache.org/repos/asf/maven/archiva/trunk@538291 13f79535-47bb-0310-9956-ffa450edef68

--- maven-meeper/src/bin/synchronize/m2-sync/conf/net.sf.jxls.sh
+++ maven-meeper/src/bin/synchronize/m2-sync/conf/net.sf.jxls.sh
@@ -4,4 +4,4 @@ CONTACTS="Leonid Vysochyn <leonid.vysochyn@gmail.com>"
 MODE=rsync_ssh
 
 FROM=mavensync@shell.sourceforge.net:/home/groups/j/jx/jxls/htdocs/repository/releases
-GROUP_DIR=net/sf/jxls
+GROUP_DIR=net/sf/jxls/
