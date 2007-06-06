584f5cf27d1245944bd03a824cdcf3a067444ef0
Carlos Sanchez Gonzalez <carlos@apache.org>
2007-06-06 17:23:02 +0000

    [MAVENUPLOAD-1585] Add net.sf.alchim sync
    git-svn-id: https://svn.apache.org/repos/asf/maven/archiva/trunk@544887 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-meeper/src/bin/synchronize/m2-sync/conf/net.sf.alchim.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+CONTACTS="David BERNARD <dwayneb@free.fr>"
+MODE=rsync_ssh
+
+FROM=mavensync@shell.sourceforge.net:/home/groups/a/al/alchim/htdocs/download/releases
+GROUP_DIR=net/sf/alchim/
