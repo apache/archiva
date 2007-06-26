25f7b3de3f4717b4d730d2566fba671376a01da7
Carlos Sanchez Gonzalez <carlos@apache.org>
2007-06-26 20:16:15 +0000

    [MAVENUPLOAD-1611] Added org.jframework sync
    git-svn-id: https://svn.apache.org/repos/asf/maven/archiva/trunk@550928 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-meeper/src/bin/synchronize/m2-sync/conf/org.jframework.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+CONTACTS="Misha Gridnev <mishalg@users.sourceforge.net>"
+MODE=rsync_ssh
+
+FROM=mavensync@shell.sf.net:/home/groups/j/jf/jframework/repository
+GROUP_DIR=org/jframework/
