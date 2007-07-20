d4c22486c206ce0a69859eb23cf2e00803ab0fe9
Carlos Sanchez Gonzalez <carlos@apache.org>
2007-07-20 07:39:16 +0000

    [MAVENUPLOAD-1644] Add net.smartlab.config
    git-svn-id: https://svn.apache.org/repos/asf/maven/archiva/trunk@557908 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-meeper/src/bin/synchronize/m2-sync/conf/net.smartlab.config.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+CONTACTS="Antonio Talarico <atalarico@users.sourceforge.net >"
+MODE=rsync_ssh
+
+FROM=mavensync@shell.sourceforge.net:/home/groups/s/sm/smartconfig/htdocs/releases
+GROUP_DIR=net/smartlab/config/
