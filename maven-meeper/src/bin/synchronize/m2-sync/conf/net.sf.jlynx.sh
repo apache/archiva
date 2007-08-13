ba2f6466b472a41bd450a346925e4c8707a714a5
Carlos Sanchez Gonzalez <carlos@apache.org>
2007-08-13 21:40:22 +0000

    [MAVENUPLOAD-1664] Fixed folder
    git-svn-id: https://svn.apache.org/repos/asf/maven/archiva/trunk@565540 13f79535-47bb-0310-9956-ffa450edef68

--- maven-meeper/src/bin/synchronize/m2-sync/conf/net.sf.jlynx.sh
+++ maven-meeper/src/bin/synchronize/m2-sync/conf/net.sf.jlynx.sh
@@ -3,5 +3,5 @@
 CONTACTS="Greg Ritchie <gregr20@gmail.com>"
 MODE=rsync_ssh
 
-FROM=mavensync@shell.sourceforge.net:/home/groups/j/jl/jlynx/htdocs/m2/repos
+FROM=mavensync@shell.sourceforge.net:/home/groups/j/jl/jlynx/htdocs/repos
 GROUP_DIR=net/sf/jlynx/
