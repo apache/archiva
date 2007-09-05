d1ddcbfa1d7af809ece7b3ee1289ad5d87ec18e3
Carlos Sanchez Gonzalez <carlos@apache.org>
2007-09-05 18:31:59 +0000

    [MAVENUPLOAD-1702] Add net.sf.tacos sync
    git-svn-id: https://svn.apache.org/repos/asf/maven/archiva/trunk@573026 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-meeper/src/bin/synchronize/m2-sync/conf/net.sf.tacos.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+CONTACTS="Igor Drobiazko <igor.drobiazko@gmail.com>"
+MODE=rsync_ssh
+
+FROM=mavensync@shell.sourceforge.net:/home/groups/t/ta/tacos/htdocs/repository
+GROUP_DIR=net/sf/tacos/
