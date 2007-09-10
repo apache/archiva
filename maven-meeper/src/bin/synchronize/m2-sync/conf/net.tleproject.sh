838776525fa57f8013aafae000e863180d47962d
Carlos Sanchez Gonzalez <carlos@apache.org>
2007-09-10 23:09:26 +0000

    [MAVENUPLOAD-1705][MAVENUPLOAD-1707][MAVENUPLOAD-1708]
    git-svn-id: https://svn.apache.org/repos/asf/maven/archiva/trunk@574402 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-meeper/src/bin/synchronize/m2-sync/conf/net.tleproject.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+CONTACTS="Choi Beom Kyun <madvirus2@gmail.com>"
+MODE=rsync_ssh
+
+FROM=maven@www.tleproject.net:~/mvnrepo
+GROUP_DIR=net/tleproject/
---
+++ maven-meeper/src/bin/synchronize/m2-sync/conf/org.jibx.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+CONTACTS="Jerome Bernard <jerome.bernard@gmail.com>"
+MODE=rsync_ssh
+
+FROM=mavensync@shell.sourceforge.net:/home/groups/j/ji/jibx/htdocs/maven2
+GROUP_DIR=org/jibx/
