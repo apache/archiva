ff427e6ff3fd45bb7bab0e48cac971780ab89693
Carlos Sanchez Gonzalez <carlos@apache.org>
2007-10-03 22:59:07 +0000

    Update svn url
    git-svn-id: https://svn.apache.org/repos/asf/maven/archiva/trunk@581742 13f79535-47bb-0310-9956-ffa450edef68

--- maven-meeper/src/bin/synchronize/m2-sync/conf/org.springframework.sh
+++ maven-meeper/src/bin/synchronize/m2-sync/conf/org.springframework.sh
@@ -2,7 +2,7 @@
 
 CONTACTS="Ben Hale <bhale@interface21.com>" 
 MODE=svn
-SVN_URL=https://svn.sourceforge.net/svnroot/springframework/repos/repo
+SVN_URL=https://springframework.svn.sourceforge.net/svnroot/springframework/repos/repo
 
 FROM=/home/maven/repository-staging/to-ibiblio/maven-spring
 GROUP_DIR=org/springframework/
