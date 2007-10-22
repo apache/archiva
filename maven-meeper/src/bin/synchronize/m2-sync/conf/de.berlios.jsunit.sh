2e968e0e085d0f7866a5c9abe6f945dd4a96156e
Carlos Sanchez Gonzalez <carlos@apache.org>
2007-10-22 23:28:03 +0000

    [MAVENUPLOAD-1774] Add de.berlios.jsunit sync
    git-svn-id: https://svn.apache.org/repos/asf/maven/archiva/trunk@587285 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-meeper/src/bin/synchronize/m2-sync/conf/de.berlios.jsunit.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+CONTACTS="Joerg Schaible <joerg.schaible@gmx.de>"
+MODE=rsync_ssh
+
+FROM=mavensync@shell.berlios.de:/home/groups/jsunit/htdocs/maven2/repo
+GROUP_DIR=de/berlios/jsunit/
