0605ff44add48cb603ceb0e4527ebbef267c61d4
Carlos Sanchez Gonzalez <carlos@apache.org>
2007-10-30 20:00:04 +0000

    [MAVENUPLOAD-1787] Add org.dspace sync
    git-svn-id: https://svn.apache.org/repos/asf/maven/archiva/trunk@590350 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-meeper/src/bin/synchronize/m2-sync/conf/org.dspace.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+CONTACTS="DSpace Developers <dspace-devel@lists.sourceforge.net>"
+MODE=rsync_ssh
+
+FROM=mavensync@shell.sourceforge.net:/home/groups/d/ds/dspace/htdocs/repository/release
+GROUP_DIR=org/dspace/
