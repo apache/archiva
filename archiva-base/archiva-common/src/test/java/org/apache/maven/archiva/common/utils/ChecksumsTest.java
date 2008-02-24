7151c6cada4f48376e0a93aa96c7b6c66c4711cd
Joakim Erdfelt <joakime@apache.org>
2008-02-24 04:33:07 +0000

    MRM-709 - Use commons-io instead of PlexusUtils. * Adding commons-io to archiva-commons deps. * Changing FileUtils to commons-io equivalents.
    git-svn-id: https://svn.apache.org/repos/asf/maven/archiva/trunk@630585 13f79535-47bb-0310-9956-ffa450edef68

--- archiva-base/archiva-common/src/test/java/org/apache/maven/archiva/common/utils/ChecksumsTest.java
+++ archiva-base/archiva-common/src/test/java/org/apache/maven/archiva/common/utils/ChecksumsTest.java
@@ -19,8 +19,8 @@ package org.apache.maven.archiva.common.utils;
  * under the License.
  */
 
+import org.apache.commons.io.FileUtils;
 import org.codehaus.plexus.PlexusTestCase;
-import org.codehaus.plexus.util.FileUtils;
 
 import java.io.BufferedReader;
 import java.io.File;
