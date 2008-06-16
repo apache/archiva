1e08f8f6b639b0cf87c996eb1dfec80529c9ad65
Brett Porter <brett@apache.org>
2008-06-16 00:03:23 +0000

    fix headers
    git-svn-id: https://svn.apache.org/repos/asf/archiva/trunk@668024 13f79535-47bb-0310-9956-ffa450edef68

--- archiva-modules/archiva-web/archiva-webdav/src/test/java/org/apache/maven/archiva/webdav/util/MimeTypesTest.java
+++ archiva-modules/archiva-web/archiva-webdav/src/test/java/org/apache/maven/archiva/webdav/util/MimeTypesTest.java
@@ -1,3 +1,5 @@
+package org.apache.maven.archiva.webdav.util;
+
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -16,8 +18,6 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
-package org.apache.maven.archiva.webdav.util;
 
 import org.codehaus.plexus.spring.PlexusInSpringTestCase;
 
