8430a3b4494f47392abf4a6b18a5c2fe82020688
Brett Porter <brett@apache.org>
2008-10-16 07:57:12 +0000

    remove merged branch
    git-svn-id: https://svn.apache.org/repos/asf/archiva/branches@705163 13f79535-47bb-0310-9956-ffa450edef68

--- archiva-MRM-239/maven-meeper/src/bin/synchronize/m2-sync/conf/net.sourceforge.openutils.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-CONTACTS="fabrizio giustina <fgiust@gmail.com>"
-MODE=rsync_ssh
-
-FROM=mavensync@shell.sourceforge.net:/home/groups/o/op/openutils/htdocs/repository/releases
-GROUP_DIR=net/sourceforge/openutils/
