0ad6879ae768bedafacd4b69602f276086f5c001
Brett Porter <brett@apache.org>
2008-10-20 12:35:48 +0000

    merge remaining changes from archiva-database-decoupling branch
    git-svn-id: https://svn.apache.org/repos/asf/archiva/branches/MRM-976@706263 13f79535-47bb-0310-9956-ffa450edef68

---
+++ archiva-modules/archiva-base/archiva-event/src/main/java/org/apache/archiva/event/EventBus.java
@@ -0,0 +1,53 @@
+package org.apache.archiva.event;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.util.Set;
+
+/**
+ * Allows implementer to emit to, subscribe and unsubscribe EventObservers
+ */
+public interface EventBus
+{
+    /**
+     * Emit a event
+     * @param emitter
+     * @param message
+     */
+    void emit(EventEmitter emitter, EventMessage message);
+
+    /**
+     * Allows the subscriber to receive messages from this event bus
+     * @param observer
+     */
+    void subscribe(EventObserver observer);
+
+    /**
+     * Stops the observer from receiving any messages
+     * @param observer
+     */
+    void unsubscribe(EventObserver observer);
+
+    /**
+     * Get the set of registered EventObservers
+     * @return
+     */
+    Set<EventObserver> getObservers();
+}
