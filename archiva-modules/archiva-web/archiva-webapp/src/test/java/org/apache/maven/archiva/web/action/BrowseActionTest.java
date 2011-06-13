4db8075bc60e3b284a346740140df9c69df6ce61
Olivier Lamy <olamy@apache.org>
2011-06-13 22:23:33 +0000

    fix more unit tests
    git-svn-id: https://svn.apache.org/repos/asf/archiva/trunk@1135326 13f79535-47bb-0310-9956-ffa450edef68

--- archiva-modules/archiva-web/archiva-webapp/src/test/java/org/apache/maven/archiva/web/action/BrowseActionTest.java
+++ archiva-modules/archiva-web/archiva-webapp/src/test/java/org/apache/maven/archiva/web/action/BrowseActionTest.java
@@ -391,7 +391,7 @@ public class BrowseActionTest
     {
         super.setUp();
         //action = (BrowseAction) lookup( Action.class, ACTION_HINT );
-        action = (BrowseAction) getActionProxy( "browse.action" ).getAction();
+        action = (BrowseAction) getActionProxy( "/browse.action" ).getAction();
         metadataResolver = new TestMetadataResolver();
         RepositorySession repositorySession = mock( RepositorySession.class );
         when( repositorySession.getResolver() ).thenReturn( metadataResolver );
