9799b541abad00dfff25609d075e2e9fa13d97eb
Olivier Lamy <olamy@apache.org>
2011-09-06 08:18:43 +0000

    formatting : no code changed
    git-svn-id: https://svn.apache.org/repos/asf/archiva/trunk@1165560 13f79535-47bb-0310-9956-ffa450edef68

--- archiva-modules/archiva-base/archiva-consumers/archiva-consumer-api/src/main/java/org/apache/maven/archiva/consumers/RepositoryContentConsumer.java
+++ archiva-modules/archiva-base/archiva-consumers/archiva-consumer-api/src/main/java/org/apache/maven/archiva/consumers/RepositoryContentConsumer.java
@@ -25,41 +25,43 @@ import java.util.Date;
 import java.util.List;
 
 /**
- * A consumer of content (files) in the repository. 
+ * A consumer of content (files) in the repository.
  *
  * @version $Id$
  */
-public interface RepositoryContentConsumer extends Consumer
+public interface RepositoryContentConsumer
+    extends Consumer
 {
     /**
      * Get the list of included file patterns for this consumer.
-     * 
+     *
      * @return the list of {@link String} patterns. (example: <code>"**<span />/*.pom"</code>)
      */
-    public List<String> getIncludes();
-    
+    List<String> getIncludes();
+
     /**
      * Get the list of excluded file patterns for this consumer.
-     * 
+     *
      * @return the list of {@link String} patterns. (example: <code>"**<span />/*.pom"</code>) - (can be null for no exclusions)
      */
-    public List<String> getExcludes();
+    List<String> getExcludes();
 
     /**
      * <p>
      * Event that triggers at the beginning of a scan.
      * </p>
-     * 
+     * <p/>
      * <p>
      * NOTE: This would be a good place to initialize the consumer, to lock any resources, and to
      * generally start tracking the scan as a whole.
      * </p>
-     * 
-     * @param repository the repository that this consumer is being used for.
+     *
+     * @param repository   the repository that this consumer is being used for.
      * @param whenGathered the start of the repository scan
      * @throws ConsumerException if there was a problem with using the provided repository with the consumer.
      */
-    public void beginScan( ManagedRepositoryConfiguration repository, Date whenGathered ) throws ConsumerException;
+    void beginScan( ManagedRepositoryConfiguration repository, Date whenGathered )
+        throws ConsumerException;
 
     /**
      * <p>
@@ -67,63 +69,63 @@ public interface RepositoryContentConsumer extends Consumer
      * executed on an entire repository or on a specific resource.
      * </p>
      *
-     * @see RepositoryContentConsumer#beginScan(org.apache.maven.archiva.configuration.ManagedRepositoryConfiguration, java.util.Date )
-     *
-     * @param repository the repository that this consumer is being used for.
-     * @param whenGathered the start of the repository scan
+     * @param repository          the repository that this consumer is being used for.
+     * @param whenGathered        the start of the repository scan
      * @param executeOnEntireRepo flags whether the consumer will be executed on an entire repository or just on a specific resource
      * @throws ConsumerException if there was a problem with using the provided repository with the consumer.
+     * @see RepositoryContentConsumer#beginScan(org.apache.maven.archiva.configuration.ManagedRepositoryConfiguration, java.util.Date)
      */
-    public void beginScan( ManagedRepositoryConfiguration repository, Date whenGathered, boolean executeOnEntireRepo )
+    void beginScan( ManagedRepositoryConfiguration repository, Date whenGathered, boolean executeOnEntireRepo )
         throws ConsumerException;
 
     /**
      * <p>
      * Event indicating a file is to be processed by this consumer.
-     * </p> 
-     * 
+     * </p>
+     * <p/>
      * <p>
      * NOTE: The consumer does not need to process the file immediately, can can opt to queue and/or track
-     * the files to be processed in batch.  Just be sure to complete the processing by the {@link #completeScan()} 
+     * the files to be processed in batch.  Just be sure to complete the processing by the {@link #completeScan()}
      * event.
      * </p>
-     * 
+     *
      * @param path the relative file path (in the repository) to process.
      * @throws ConsumerException if there was a problem processing this file.
      */
-    public void processFile( String path ) throws ConsumerException;
+    void processFile( String path )
+        throws ConsumerException;
 
     /**
-     *
      * @param path
      * @param executeOnEntireRepo
      * @throws Exception
      */
-    public void processFile( String path, boolean executeOnEntireRepo ) throws Exception;
-    
+    void processFile( String path, boolean executeOnEntireRepo )
+        throws Exception;
+
     /**
      * <p>
      * Event that triggers on the completion of a scan.
      * </p>
-     * 
+     * <p/>
      * <p>
      * NOTE: If the consumer opted to batch up processing requests in the {@link #processFile(String)} event
      * this would be the last opportunity to drain any processing queue's.
      * </p>
      */
-    public void completeScan();
+    void completeScan();
 
     /**
-     * 
      * @param executeOnEntireRepo
      * @throws Exception
      */
-    public void completeScan( boolean executeOnEntireRepo );
+    void completeScan( boolean executeOnEntireRepo );
 
     /**
      * Whether the consumer should process files that have not been modified since the time passed in to the scan
      * method.
+     *
      * @return whether to process the unmodified files
      */
-    boolean isProcessUnmodified();        
+    boolean isProcessUnmodified();
 }
