34d960634636a77bad37af324c5ade16263de684
Olivier Lamy <olamy@apache.org>
2011-10-05 14:49:22 +0000

    [MRM-1327] implement alternative or improve repository metadata storage: if no repository.xml found in ${appserver.base}/conf/repository.xml revert to the default found in resources.
    git-svn-id: https://svn.apache.org/repos/asf/archiva/trunk@1179245 13f79535-47bb-0310-9956-ffa450edef68

---
+++ archiva-modules/archiva-web/archiva-webapp/src/main/java/org/apache/archiva/web/startup/ArchivaJcrRepositoryConfig.java
@@ -0,0 +1,55 @@
+package org.apache.archiva.web.startup;
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.jackrabbit.core.config.ConfigurationException;
+import org.apache.jackrabbit.core.config.RepositoryConfig;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+
+/**
+ * @author Olivier Lamy
+ */
+public class ArchivaJcrRepositoryConfig
+{
+    public static RepositoryConfig create( String file, String home )
+        throws ConfigurationException
+    {
+        File homeFile = new File( home );
+        if ( !homeFile.exists( ) )
+        {
+            homeFile.mkdirs( );
+
+        }
+
+        File configurationFile = new File( file );
+        if ( !configurationFile.exists( ) )
+        {
+            String resourcePath = "org/apache/archiva/repository.xml";
+            LoggerFactory.getLogger( ArchivaJcrRepositoryConfig.class ).info(
+                "now repository.xml file in path {} so use default from resources path {}", file, resourcePath );
+            // use bundled repository.xml
+            return RepositoryConfig.create(
+                Thread.currentThread( ).getContextClassLoader( ).getResourceAsStream( resourcePath ), home );
+        }
+
+        return RepositoryConfig.create( file, home );
+    }
+}
