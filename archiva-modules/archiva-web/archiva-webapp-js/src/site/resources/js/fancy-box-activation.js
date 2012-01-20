1c363e84e93a5684a3d72b7bc0a4ccb8a2fa9ba2
Olivier Lamy <olamy@apache.org>
2012-01-20 23:18:19 +0000

    configure fancybox to display images
    git-svn-id: https://svn.apache.org/repos/asf/archiva/trunk@1234203 13f79535-47bb-0310-9956-ffa450edef68

---
+++ archiva-modules/archiva-web/archiva-webapp-js/src/site/resources/js/fancy-box-activation.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+ $("a.single_image").fancybox({
+   'transitionIn'	:	'elastic',
+   'transitionOut'	:	'elastic',
+   'speedIn'		:	600,
+   'speedOut'		:	200,
+   'overlayShow'	:	true
+ });
+ $("a.gallery_image").fancybox({
+   'transitionIn'	:	'elastic',
+   'transitionOut'	:	'elastic',
+   'speedIn'		:	600,
+   'speedOut'		:	200,
+   'overlayShow'	:	true,
+   'titlePosition': 'inside'
+ });
+});
+
