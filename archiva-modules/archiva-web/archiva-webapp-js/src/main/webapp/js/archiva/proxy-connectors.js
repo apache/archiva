d080db57e55e267c87578787e4b7972525d0f53d
Olivier Lamy <olamy@apache.org>
2012-01-30 09:47:41 +0000

    proxy connector screen add PolicyInformation bean
    git-svn-id: https://svn.apache.org/repos/asf/archiva/trunk@1237581 13f79535-47bb-0310-9956-ffa450edef68

--- archiva-modules/archiva-web/archiva-webapp-js/src/main/webapp/js/archiva/proxy-connectors.js
+++ archiva-modules/archiva-web/archiva-webapp-js/src/main/webapp/js/archiva/proxy-connectors.js
@@ -31,9 +31,9 @@ $(function() {
     //private List<String> whiteListPatterns;
     this.whiteListPatterns=ko.observableArray(whiteListPatterns);
     //private Map<String, String> policies;
-
+   this.policies=ko.observable(policies);
     //private Map<String, String> properties;
-
+    this.properties=ko.observable(properties);
     //private boolean disabled = false;
     this.disabled=ko.observable(disabled);
     //private int order = 0;
@@ -43,6 +43,113 @@ $(function() {
   displayProxyConnectors=function(){
     $("#main-content").html($("#proxyConnectorsMain").tmpl());
     $("#main-content").append(smallSpinnerImg());
+  PolicyInformation=function(options,defaultOption,id,name){
+    //private List<String> options;
+    this.options=ko.observableArray(options);
+    //private String defaultOption;
+    this.defaultOption=ko.observable(defaultOption);
+    //private String id;
+    this.id=ko.observable(id);
+    //private String name;
+    this.name=ko.observable(name);
+  }
+
+  ProxyConnectorsViewModel=function(){
+    this.proxyConnectors=ko.observableArray([]);
+    var self=this;
+
+    editProxyConnector=function(proxyConnector){
+
+    }
+
+    this.displayGrid=function(){
+      var sourcesRepos=[];
+      //sourceRepoId
+      for(i=0;i<self.proxyConnectors().length;i++){
+        var curSrcRepo=self.proxyConnectors()[i].sourceRepoId();
+        var curTarget=self.proxyConnectors()[i].targetRepoId;
+        var sourceRepo = $.grep(sourcesRepos,
+                                function(srcRepo,idx){
+                                  $.log("grep:"+srcRepo.source);
+                                  $.log("sourcesRepos.length:"+sourcesRepos.length);
+                                  for (i=0;i<sourcesRepos.length;i++){
+                                    if (srcRepo.source==sourcesRepos[i].source){
+                                      return true;
+                                    }
+                                  }
+                                  return false;
+                                }
+        );
+        if (sourceRepo!=null){
+          $.log("sourceRepoId:"+curSrcRepo);
+          sourcesRepos.push({source:curSrcRepo,targetRepos:[curTarget]});
+        } else {
+          sourceRepo.targetRepos.push(curTarget);
+        }
+      }
+
+      $.log("sourcesRepo.length:"+sourcesRepos.length);
+      for(i=0;i<sourcesRepos.length;i++){
+        $.log(sourcesRepos.targetRepos.length);
+      }
+    }
+  }
+
+  displayProxyConnectors=function(){
+    $("#main-content").html($("#proxyConnectorsMain").tmpl());
+    $("#main-content").append(smallSpinnerImg());
+
+    var proxyConnectorsViewModel = new ProxyConnectorsViewModel();
+
+    $.ajax("restServices/archivaServices/proxyConnectorService/getProxyConnectors", {
+        type: "GET",
+        dataType: 'json',
+        success: function(data) {
+          proxyConnectorsViewModel.proxyConnectors(mapProxyConnectors(data));
+          proxyConnectorsViewModel.displayGrid();
+        }
+      }
+    );
+
+    $.ajax("restServices/archivaServices/proxyConnectorService/allPolicies", {
+        type: "GET",
+        dataType: 'json',
+        success: function(data) {
+          mapPolicyInformations(data);
+        }
+      }
+    );
+
+
+  }
+
+  mapProxyConnector=function(data){
+    if (data==null){
+      return null;
+    }
+    return new ProxyConnector(data.sourceRepoId,data.targetRepoId,data.proxyId,mapStringArray(data.blackListPatterns),
+                              mapStringArray(data.whiteListPatterns),data.policies,data.properties,
+                              data.disabled,data.order);
+  }
+
+  mapProxyConnectors=function(data){
+    var mappedProxyConnectors = $.map(data.proxyConnector, function(item) {
+      return mapProxyConnector(item);
+    });
+    return mappedProxyConnectors;
+  }
+
+  mapPolicyInformation=function(data){
+    if (data==null){
+      return null;
+    }
+    return new PolicyInformation(mapStringArray(data.options),data.defaultOption,data.id,data.name);
+  }
+
+  mapPolicyInformations=function(data){
+    return $.map(data.policyInformation, function(item) {
+          return mapPolicyInformation(item);
+        });
   }
 
 });
