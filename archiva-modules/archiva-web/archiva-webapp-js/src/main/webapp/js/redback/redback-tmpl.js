0dc4a69e7aec7a71410a7c6d46690fa12ddf96bc
Olivier Lamy <olamy@apache.org>
2012-03-23 14:52:57 +0000

    no need of 2 ? in url
    git-svn-id: https://svn.apache.org/repos/asf/archiva/trunk@1304388 13f79535-47bb-0310-9956-ffa450edef68

--- archiva-modules/archiva-web/archiva-webapp-js/src/main/webapp/js/redback/redback-tmpl.js
+++ archiva-modules/archiva-web/archiva-webapp-js/src/main/webapp/js/redback/redback-tmpl.js
@@ -16,13 +16,13 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-require(["text!js/redback/templates/user-edit.html?"+appendTemplateUrl(),
-          "text!js/redback/templates/user-grids.html?"+appendTemplateUrl(),
-          "text!js/redback/templates/login.html?"+appendTemplateUrl(),
-          "text!js/redback/templates/register-form.html?"+appendTemplateUrl(),
-          "text!js/redback/templates/password-change-form.html?"+appendTemplateUrl(),
-          "text!js/redback/templates/user-edit-form.html?"+appendTemplateUrl(),
-          "text!js/redback/templates/roles-tmpl.html?"+appendTemplateUrl()],
+require(["text!js/redback/templates/user-edit.html"+appendTemplateUrl(),
+          "text!js/redback/templates/user-grids.html"+appendTemplateUrl(),
+          "text!js/redback/templates/login.html"+appendTemplateUrl(),
+          "text!js/redback/templates/register-form.html"+appendTemplateUrl(),
+          "text!js/redback/templates/password-change-form.html"+appendTemplateUrl(),
+          "text!js/redback/templates/user-edit-form.html"+appendTemplateUrl(),
+          "text!js/redback/templates/roles-tmpl.html"+appendTemplateUrl()],
     function(useredit, usergrids, login,register,passwordchange,usereditform,roles) {
 
       var htmlFragment=$("#html-fragments");
