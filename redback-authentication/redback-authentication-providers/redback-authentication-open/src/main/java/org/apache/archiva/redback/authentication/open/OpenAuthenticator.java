9a415093c21143d3b2fc940f31867fef2c0019b3
Olivier Lamy <olamy@apache.org>
2012-04-07 20:41:57 +0000

    package move to o.a.a.r module redback-authentication-open
    git-svn-id: https://svn.apache.org/repos/asf/archiva/redback/redback-core/trunk@1310860 13f79535-47bb-0310-9956-ffa450edef68

--- redback-authentication/redback-authentication-providers/redback-authentication-open/src/main/java/org/apache/archiva/redback/authentication/open/OpenAuthenticator.java
+++ redback-authentication/redback-authentication-providers/redback-authentication-open/src/main/java/org/apache/archiva/redback/authentication/open/OpenAuthenticator.java
@@ -1,4 +1,4 @@
-package org.codehaus.plexus.redback.authentication.open;
+package org.apache.archiva.redback.authentication.open;
 
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
