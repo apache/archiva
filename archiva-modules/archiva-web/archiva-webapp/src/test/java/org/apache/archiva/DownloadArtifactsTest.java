621340f55222be25b7e6785c11ea52b3148e8b8d
Olivier Lamy <olamy@apache.org>
2012-04-08 20:28:25 +0000

    move to package o.a.a.r redback module redback-rest-api
    git-svn-id: https://svn.apache.org/repos/asf/archiva/trunk@1311084 13f79535-47bb-0310-9956-ffa450edef68

--- archiva-modules/archiva-web/archiva-webapp/src/test/java/org/apache/archiva/DownloadArtifactsTest.java
+++ archiva-modules/archiva-web/archiva-webapp/src/test/java/org/apache/archiva/DownloadArtifactsTest.java
@@ -23,7 +23,7 @@ import org.apache.archiva.security.common.ArchivaRoleConstants;
 import org.apache.commons.io.FileUtils;
 import org.apache.maven.wagon.providers.http.HttpWagon;
 import org.apache.maven.wagon.repository.Repository;
-import org.codehaus.redback.rest.api.services.RoleManagementService;
+import org.apache.archiva.redback.rest.api.services.RoleManagementService;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
