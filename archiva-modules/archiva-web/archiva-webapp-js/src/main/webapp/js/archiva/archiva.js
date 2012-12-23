09eda07d304f36f910b6d79ec8badb588bc53996
Olivier Lamy <olamy@apache.org>
2012-12-23 16:39:35 +0000

    jquery 1.8.3
    git-svn-id: https://svn.apache.org/repos/asf/archiva/trunk@1425476 13f79535-47bb-0310-9956-ffa450edef68

--- archiva-modules/archiva-web/archiva-webapp-js/src/main/webapp/js/archiva/archiva.js
+++ archiva-modules/archiva-web/archiva-webapp-js/src/main/webapp/js/archiva/archiva.js
@@ -58,7 +58,7 @@ $.ajax({
                       },
                 paths: {
                     "i18n":"jquery.i18n.properties-1.0.9",
-                    "jquery": "jquery-1.8.1.min",
+                    "jquery": "jquery-1.8.3.min",
                     "jquery.tmpl": "jquery.tmpl",
                     "utils": "archiva/utils",
                     "startup": "archiva/startup",
