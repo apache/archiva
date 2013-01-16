0c104fb59a699ae13aecba891b1da0430431d8a9
Olivier Lamy <olamy@apache.org>
2013-01-16 15:20:38 +0000

    typo
    git-svn-id: https://svn.apache.org/repos/asf/archiva/redback/redback-core/trunk@1433979 13f79535-47bb-0310-9956-ffa450edef68

--- redback-common/redback-common-ldap/src/main/java/org/apache/archiva/redback/common/ldap/role/DefaultLdapRoleMapper.java
+++ redback-common/redback-common-ldap/src/main/java/org/apache/archiva/redback/common/ldap/role/DefaultLdapRoleMapper.java
@@ -187,7 +187,7 @@ public class DefaultLdapRoleMapper
             }
             else
             {
-                log.warn( "skip group creation as no mapping fro roleName:'{}'", roleName );
+                log.warn( "skip group creation as no mapping for roleName:'{}'", roleName );
                 return false;
             }
         }
