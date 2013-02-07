47b902d8abefc93058de63c7ad41b60b678d2104
Olivier Lamy <olamy@apache.org>
2013-02-07 14:58:12 +0000

    better with adding the new class
    git-svn-id: https://svn.apache.org/repos/asf/archiva/trunk@1443528 13f79535-47bb-0310-9956-ffa450edef68

---
+++ archiva-modules/archiva-base/archiva-indexer/src/main/java/org/apache/archiva/indexer/merger/IndexMergerRequest.java
@@ -0,0 +1,91 @@
+package org.apache.archiva.indexer.merger;
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.util.Collection;
+
+/**
+ * @author Olivier Lamy
+ */
+public class IndexMergerRequest
+{
+    /**
+     * repositories Ids to merge content
+     */
+    private Collection<String> repositoriesIds;
+
+    /**
+     * will generate a downloadable index
+     */
+    private boolean packIndex;
+
+    /**
+     * original groupId (repositoryGroup id)
+     */
+    private String groupId;
+
+    public IndexMergerRequest( Collection<String> repositoriesIds, boolean packIndex, String groupId )
+    {
+        this.repositoriesIds = repositoriesIds;
+        this.packIndex = packIndex;
+        this.groupId = groupId;
+    }
+
+    public Collection<String> getRepositoriesIds()
+    {
+        return repositoriesIds;
+    }
+
+    public void setRepositoriesIds( Collection<String> repositoriesIds )
+    {
+        this.repositoriesIds = repositoriesIds;
+    }
+
+    public boolean isPackIndex()
+    {
+        return packIndex;
+    }
+
+    public void setPackIndex( boolean packIndex )
+    {
+        this.packIndex = packIndex;
+    }
+
+    public String getGroupId()
+    {
+        return groupId;
+    }
+
+    public void setGroupId( String groupId )
+    {
+        this.groupId = groupId;
+    }
+
+    @Override
+    public String toString()
+    {
+        final StringBuilder sb = new StringBuilder();
+        sb.append( "IndexMergerRequest" );
+        sb.append( "{repositoriesIds=" ).append( repositoriesIds );
+        sb.append( ", packIndex=" ).append( packIndex );
+        sb.append( ", groupId='" ).append( groupId ).append( '\'' );
+        sb.append( '}' );
+        return sb.toString();
+    }
+}
