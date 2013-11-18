a4c7b97e8d93afdd69920d852e89d41990d3f8cb
Olivier Lamy <olamy@apache.org>
2013-11-18 05:47:58 +0000

    add new classes
    git-svn-id: https://svn.apache.org/repos/asf/archiva/trunk@1542900 13f79535-47bb-0310-9956-ffa450edef68

---
+++ archiva-modules/plugins/metadata-store-cassandra/src/main/java/org/apache/archiva/metadata/repository/cassandra/CassandraUtils.java
@@ -0,0 +1,64 @@
+package org.apache.archiva.metadata.repository.cassandra;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/**
+ * @author Olivier Lamy
+ */
+public class CassandraUtils
+{
+
+    private static final String EMPTY_VALUE = "";
+
+    public static final String SEPARATOR = "->";
+
+    public static String generateKey( final String... bases )
+    {
+        final StringBuilder builder = new StringBuilder();
+        if ( bases == null || bases.length == 0 )
+        {
+            return builder.toString();
+        }
+
+        for ( final String s : bases )
+        {
+            if ( s != null )
+            {
+                builder.append( s );
+            }
+            else
+            {
+                builder.append( EMPTY_VALUE );
+            }
+            builder.append( SEPARATOR );
+        }
+        if ( builder.length() > 0 )
+        {
+            builder.setLength( builder.length() - SEPARATOR.length() );
+        }
+        return builder.toString();
+    }
+
+    private CassandraUtils()
+    {
+        // no-op
+    }
+
+}
