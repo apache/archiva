946557f1648025a37476dddf7691bd829114c599
Olivier Lamy <olamy@apache.org>
2014-03-28 11:43:34 +0000

    more metadata store cassandra to cassandra profile as need 1.7 dependency
    git-svn-id: https://svn.apache.org/repos/asf/archiva/trunk@1582714 13f79535-47bb-0310-9956-ffa450edef68

--- archiva-modules/plugins/metadata-store-cassandra/src/main/java/org/apache/archiva/metadata/repository/cassandra/CassandraRepositorySessionFactory.java
+++ archiva-modules/plugins/metadata-store-cassandra/src/main/java/org/apache/archiva/metadata/repository/cassandra/CassandraRepositorySessionFactory.java
@@ -71,8 +71,6 @@ public class CassandraRepositorySessionFactory
         {
             metadataFacetFactories.put( StringUtils.substringAfterLast( entry.getKey(), "#" ), entry.getValue() );
         }
-
-
     }
 
 
