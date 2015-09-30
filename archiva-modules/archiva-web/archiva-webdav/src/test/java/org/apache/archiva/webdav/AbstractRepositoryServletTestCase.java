6484df9d69b4e359f6fa43af8f6d898ab33d9801
Olivier Lamy <olamy@apache.org>
2015-09-30 22:26:21 +1000

    fix compilation

--- archiva-modules/archiva-web/archiva-webdav/src/test/java/org/apache/archiva/webdav/AbstractRepositoryServletTestCase.java
+++ archiva-modules/archiva-web/archiva-webdav/src/test/java/org/apache/archiva/webdav/AbstractRepositoryServletTestCase.java
@@ -51,6 +51,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.MessageSourceResolvable;
 import org.springframework.context.NoSuchMessageException;
+import org.springframework.core.ResolvableType;
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.Resource;
 import org.springframework.mock.web.MockHttpServletRequest;
@@ -423,6 +424,25 @@ public abstract class AbstractRepositoryServletTestCase
             throws IOException
         {
             return applicationContext.getResources( s );
+        }
+
+        @Override
+        public void publishEvent( Object o )
+        {
+            // no op
+        }
+
+        @Override
+        public String[] getBeanNamesForType( ResolvableType resolvableType )
+        {
+            return new String[0];
+        }
+
+        @Override
+        public boolean isTypeMatch( String s, ResolvableType resolvableType )
+            throws NoSuchBeanDefinitionException
+        {
+            return false;
         }
 
         @Override
