a02f98c6e26c996e23a667d4e0cbab57def04c84
Martin Stockhammer <martin_s@apache.org>
2017-04-30 17:43:23 +0200

    Additional test

--- src/ci/scripts/cleanup.ps1
+++ src/ci/scripts/cleanup.ps1
@@ -2,9 +2,20 @@
     Powershell script for cleaning up remaining processes on the CI servers
 #>
 
-$list = dir
-foreach ($item in $list) {
-     $fn = $item.name + "_.txt"
-     get-itemproperty $item | format-list 
+Get-Process 
+
+$processes = Get-WmiObject Win32_Process -Filter "name = 'java.exe'"
+foreach($proc in $processes)
+{
+    if($proc.CommandLine.Contains("selenium-server.jar"))
+    {
+        Write-Host "stopping proccess $($proc.ProcessId) with $($proc.ThreadCount) threads; $($proc.CommandLine.Substring(0, 50))..."
+        Stop-Process -F $proc.ProcessId
+    } else
+    {
+        Write-Host "skipping proccess $($proc.ProcessId) with $($proc.ThreadCount) threads; $($proc.CommandLine.Substring(0, 50))..."
+    }
 }
-Get-Process -IncludeUserName 
+Get-Process firefox | Stop-Process
+Get-Process chrome | Stop-Process
+Get-Process iexplore | Stop-Process
