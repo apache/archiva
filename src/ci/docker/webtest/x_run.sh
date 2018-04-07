6b1350b3c08c380222e6f0dfb350f6e31dc7e466
Martin Stockhammer <martin_s@apache.org>
2018-04-07 13:15:43 +0200

    Adding license text

--- src/ci/docker/webtest/x_run.sh
+++ src/ci/docker/webtest/x_run.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
-
+#
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing,
+#  software distributed under the License is distributed on an
+#  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+#  KIND, either express or implied.  See the License for the
+#  specific language governing permissions and limitations
+#  under the License.
+#
+#  Author: Martin Stockhammer <martin_s@apache.org>
+#  Date:   2018-04-07
+#
+#  Only used for starting fluxbox before running selenium driver
+#  -> fluxbox is a window manager and needed for chrome and selenium
+#
 nohup fluxbox >/dev/null 2>&1 &
 sleep 3
 exec java ${JAVA_OPTS} -jar /opt/bin/selenium-server-standalone.jar ${SE_OPTS}
