a3c149327e470c0391b9807545651de38c50e728
Martin Stockhammer <martin_s@apache.org>
2019-05-04 10:48:48 +0200

    Adding utility for artifact path handling

---
+++ archiva-modules/archiva-base/archiva-repository-layer/src/main/java/org/apache/archiva/repository/content/ArtifactUtil.java
@@ -0,0 +1,56 @@
+package org.apache.archiva.repository.content;
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.archiva.model.ArtifactReference;
+import org.apache.archiva.repository.ManagedRepository;
+import org.apache.archiva.repository.ManagedRepositoryContent;
+import org.apache.archiva.repository.RepositoryContentFactory;
+import org.apache.archiva.repository.RepositoryException;
+import org.springframework.stereotype.Service;
+
+import javax.inject.Inject;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Utility class that gives information about the physical location of artifacts.
+ */
+@Service( "ArtifactUtil#default" )
+public class ArtifactUtil {
+
+    @Inject
+    RepositoryContentFactory repositoryContentFactory;
+
+    /**
+     * Returns the physical location of a given artifact in the repository. There is no check for the
+     * existence of the returned file.
+     *
+     * @param repository The repository, where the artifact is stored.
+     * @param artifactReference The artifact reference.
+     * @return The absolute path to the artifact.
+     * @throws RepositoryException
+     */
+    public Path getArtifactPath(ManagedRepository repository, ArtifactReference artifactReference) throws RepositoryException {
+        final ManagedRepositoryContent content = repositoryContentFactory.getManagedRepositoryContent(repository);
+        final String artifactPath = content.toPath( artifactReference );
+        return Paths.get(repository.getLocation()).resolve(artifactPath);
+    }
+
+}
