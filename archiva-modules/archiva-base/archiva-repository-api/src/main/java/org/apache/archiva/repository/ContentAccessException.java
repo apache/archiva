13856a6060f158ca5fac07d2269adb7fdfd87aef
Martin Stockhammer <martin_s@apache.org>
2020-02-10 23:21:15 +0100

    Adding exception for IO errors

---
+++ archiva-modules/archiva-base/archiva-repository-api/src/main/java/org/apache/archiva/repository/ContentAccessException.java
@@ -0,0 +1,49 @@
+package org.apache.archiva.repository;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/**
+ * This exception is thrown, during errors while accessing the repository data
+ * E.g. the cause may be IO errors while accessing the filesystem, permission problems
+ * on the filesystem or other backend related problems.
+ */
+public class ContentAccessException extends RepositoryException
+{
+    private static final long serialVersionUID = 3811491193671356230L;
+
+    public ContentAccessException( )
+    {
+    }
+
+    public ContentAccessException( String message, Throwable cause )
+    {
+        super( message, cause );
+    }
+
+    public ContentAccessException( String message )
+    {
+        super( message );
+    }
+
+    public ContentAccessException( Throwable cause )
+    {
+        super( cause );
+    }
+}
