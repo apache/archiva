448470b3dfd15c192d172cb0058d3108e9867cdb
Olivier Lamy <olamy@apache.org>
2023-06-09 07:31:05 +1000

    fix compilation
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- archiva-modules/archiva-web/archiva-webdav/src/test/java/org/apache/archiva/webdav/ArchivaDavSessionProviderTest.java
+++ archiva-modules/archiva-web/archiva-webdav/src/test/java/org/apache/archiva/webdav/ArchivaDavSessionProviderTest.java
@@ -47,7 +47,6 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.servlet.http.HttpUpgradeHandler;
 import javax.servlet.http.Part;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -111,21 +110,6 @@ public class ArchivaDavSessionProviderTest
             throws IOException, ServletException
         {
             throw new UnsupportedOperationException( "Not supported yet." );
-        }
-
-        @Override
-        public String changeSessionId() {
-            return null;
-        }
-
-        @Override
-        public <T extends HttpUpgradeHandler> T upgrade(Class<T> handlerClass) throws IOException, ServletException {
-            return null;
-        }
-
-        @Override
-        public long getContentLengthLong() {
-            return 0;
         }
 
         @Override
